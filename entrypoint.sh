1b5ce5861bf25e5807cac8475aef6581bc79698a
Jérôme Launay <jerome@projet-libre.org>
2025-11-27 20:48:28 +0100

    Add include_unread env

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ CMD="/koshelf --books-path /books --statistics-db /settings/statistics.sqlite3 -
 [ -n "$DOCSETTINGS" ] && CMD="$CMD --docsettings-path $DOCSETTINGS"
 [ -n "$HASHDOCSETTINGS" ] && CMD="$CMD --hashdocsettings-path $HASHDOCSETTINGS"
 [ -n "$TITLE" ] && CMD="$CMD --title \"$TITLE\""
+[ "$INCLUDE_UNREAD" = "true" ] && CMD="$CMD --include-unread"
 
 eval exec $CMD
